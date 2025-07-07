911957164ac238cee5eea8d69d385d446409c75c
Mocanu Cristian Nicholas <cristian_nichoals@yahoo.com>
2025-07-07 10:03:45 +0300

    Add project files.

---
+++ NumarareLiniiFisier/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+namespace NumarareLiniiFisier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string locatieFisier = @"C:\C#\ExempluProgramNumarare.docx";
+            if (File.Exists(locatieFisier))
+            {
+                string[] linii = File.ReadAllLines(locatieFisier);
+                Console.WriteLine($"Numarul de linii este {linii.Length} ");
+            }
+            else
+            {
+                Console.WriteLine("Fisierul nu exista");
+            }
+        }
+    }
+}
